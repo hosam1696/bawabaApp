091aa8f9b8509f35a93dcc23bca4f70d876cc0e4
ashraf <ashraf@ashrafs-MacBook-Pro.local>
2017-09-06 10:37:26 +0200

    change app id and version

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -143,7 +143,7 @@ export class Settings {
 
   shareApp() {
 
-    this.social.share('حمل تطبيق بوابة النقل من خلال هذه الروابط','http://is4.mzstatic.com/image/thumb/Purple18/v4/14/7c/cc/147ccc54-5384-f17e-85ab-152e5420b59f/source/175x175bb.jpg', 'https://goo.gl/W1eeRU')
+    this.social.share('حمل تطبيق بوابة النقل من خلال هذه الروابط /n للايفون https://goo.gl/W1eeRU')
     .then((data)=> {
       console.log(data);
     }
